5e3131331d57463299ab4d8d2b2031a22e272779
pab <dev.pab.2020@gmail.com>
2021-03-06 19:16:57 -0300

    add more DataTypes

--- utils/DataTypes.php
+++ utils/DataTypes.php
@@ -1,6 +1,14 @@
 <?php
     abstract class DataTypes {
         const STRING = 'VARCHAR(255)';
+        const TEXT = 'TEXT';
+        const MEDIUMTEXT = 'MEDIUMTEXT';
+        const LONGTEXT = 'LONGTEXT';
+        
         const INT = 'INT';
+        const TINYINT = 'TINYINT';
+        const SMALLINT = 'SMALLINT';
+        const MEDIUMINT = 'MEDIUMINT';
+        const FLOAT = 'FLOAT';
     }
 ?>
